eeb0aa672fd8933b53fee0d58ae208db16d89763
Samuel Belete <samyethio891@gmail.com>
2022-07-26 01:12:14 -0700

    header hero aded

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,28 @@ import "../styles/app.css";
 import React from 'react';
 import Recipes from "./Recipes";
 function App() {
-    return <div className="main">
-        <h1>Switch Users</h1>
-        <Recipes/>
-    </div>
+   return (
+		<div>
+			<div className="hero">
+				<div>
+					<div className="info">
+						<h2>OUR MISSION</h2>
+						<p>
+							Multiple separate builds should form a single
+							application. These separate builds should not have
+							dependencies between each other, so they can be
+							developed and deployed individually.
+						</p>
+						<button>Get More Info</button>
+					</div>
+				</div>
+			</div>
+			<div className="main">
+				<h1>Switch Users</h1>
+				<Recipes />
+			</div>
+		</div>
+   );
 }
 
 export default App;
